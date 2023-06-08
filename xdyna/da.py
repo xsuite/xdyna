ec655cedfc4604d1ab42388989f81ff853d8aed2
tpugnat <thomas.pugnat@gmail.com>
2023-06-08 14:19:11 +0200

    Add function loading SixDesk/SixDB outputs into XDyna

--- xdyna/da.py
+++ xdyna/da.py
@@ -2727,3 +2727,122 @@ def _da_smoothing(data,raw_border_min,raw_border_max,at_turn,removed=pd.DataFram
 
     return tmp_border_min,tmp_border_max
 # --------------------------------------------------------
+    
+    
+    
+
+# Function loading SixDesk/SixDB outputs into XDyna
+# --------------------------------------------------------
+def load_sixdesk_output(path,study,load_line=False):
+    ## SIXDESK
+    ## -----------------------------------
+    # Load meta
+    meta=pd.read_csv(path+'/'+study+".meta.csv",header=0); meta=meta.set_index('keyname')
+    
+    # Load polar
+    tp=pd.read_csv(path+'/'+study+".polar.csv",header=0)
+    polar_seed =tp.loc[:,'seed'].values
+    polar_ang  =tp.loc[:,'angle'].values
+    polar_DA_P =tp.loc[:,'alost2'].values
+    polar_DA_P1=tp.loc[:,'alost1'].values
+    
+    # Load surv
+    tp=pd.read_csv(path+'/'+study+".surv.csv",header=0)
+    surv_seed =tp.loc[:,'seed'].values
+    surv_ang  =tp.loc[:,'angle'].values
+    surv_amp  =tp.loc[:,'amp'].values
+    surv_ntrn1=tp.loc[:,'sturns1'].values
+    surv_ntrn2=tp.loc[:,'sturns2'].values
+    
+    
+    ## META
+    ## -----------------------------------
+    if not exists(Path(path,study+'.meta.json')):
+        # Generate meta class
+        sixdb_da = DA(name=study,                                   # Name of the Study
+                   path=Path(path),                              # Path to the Study (path/name.meta.json)
+                   normalised_emittance=np.float64(meta.loc['emit','value'])*1e-6,  # Normalised emittance: ne or (nex,ney) [m]
+                   max_turns=int(meta.loc['turnsl','value']), 
+                   nseeds=max(surv_seed),                        # For multiseed study (Default=0)
+                   use_files=True)
+    
+    else:
+        # Load the study metadata"
+        sixdb_da = DA(name=study, path=Path(path), use_files=True)
+    
+    
+    ## LINE
+    ## -----------------------------------
+    if sixdb_da.line_file is None and load_line:
+        # Define the line
+        sixdb_da.madx_file = Path(path,study+".mask")       # MadX File to build the line from
+        sixdb_da.line_file = Path(path,study+".line.json")  # Line File path
+
+        if not exists(sixdb_da.line_file):
+            # Set label to remove:
+            label={
+                       "%EMIT_BEAM":np.float64(meta.loc['emit','value']),  # [um]
+                       "%NPART":1,
+                       "%XING":np.float64(meta.loc['xing','value']),
+                   }
+
+            # Unmask the mask
+            with open(sixdb_da.madx_file, 'r') as fin:
+                data = fin.read()
+                for key, value in label.items():
+                    print(key, value)
+                    data=data.replace(key, str(value))
+                with open(Path(path,study+".mask.unmasked"), 'w') as fout:
+                    fout.write(data)
+            sixdb_da.madx_file = Path(path,study+".mask.unmasked")
+
+            # Build the line from MadX
+            sequence= 'lhcb1' if meta.loc['beam','value']=='B1' else 'lhcb2'
+            sixdb_da.build_line_from_madx(sequence=sequence,  run_all_seeds= (sixdb_da.meta.nseeds!=0) )
+    
+    
+    ## SURV
+    ## -----------------------------------
+    if not exists(Path(path,study+'.surv.paquet')):
+        # Load particle distribution as a polar grid.
+        x = surv_amp*np.cos(surv_ang*np.pi/180)
+        y = surv_amp*np.sin(surv_ang*np.pi/180)
+
+        sixdb_da._surv = pd.DataFrame(index=range(len(surv_amp)))
+        sixdb_da._surv.loc[:,'seed'] = surv_seed
+        sixdb_da._surv.loc[:,'ang_xy'] = surv_ang
+        sixdb_da._surv.loc[:,'r_xy'] = surv_amp
+        sixdb_da._surv.loc[:,'nturns'] = surv_ntrn1
+        sixdb_da._surv.loc[:,'x_norm_in'] = x
+        sixdb_da._surv.loc[:,'y_norm_in'] = y
+        sixdb_da._surv.loc[:,'px_norm_in'] = 0
+        sixdb_da._surv.loc[:,'py_norm_in'] = 0
+        sixdb_da._surv.loc[:,'zeta_in'] = 0
+        sixdb_da._surv.loc[:,'delta_in'] = np.float64(meta.loc['dpini','value'])
+        sixdb_da._surv.loc[:,'x_out'] = 0
+        sixdb_da._surv.loc[:,'y_out'] = 0
+        sixdb_da._surv.loc[:,'px_out'] = 0
+        sixdb_da._surv.loc[:,'py_out'] = 0
+        sixdb_da._surv.loc[:,'zeta_out'] = 0
+        sixdb_da._surv.loc[:,'delta_out'] = 0
+        sixdb_da._surv.loc[:,'s_out'] = 0
+        sixdb_da._surv.loc[:,'state'] = 1*(surv_ntrn1==sixdb_da.max_turns)
+        sixdb_da._surv.loc[:,'submitted'] = True
+        sixdb_da._surv.loc[:,'finished'] = True
+        sixdb_da.meta.pairs_shift=1
+        sixdb_da.meta.pairs_shift_var='x'
+        sixdb_da._create_pairs()
+        orig = (sixdb_da._surv['paired_to'] == sixdb_da._surv.index)
+        sixdb_da._surv.loc[~orig,'nturns'] = surv_ntrn2
+        sixdb_da._surv.loc[~orig,'state'] = 1*(surv_ntrn2==sixdb_da.max_turns)
+        sixdb_da.write_surv()
+        sixdb_da.meta.da_type = 'radial'
+        sixdb_da.meta.da_dim = 2
+        sixdb_da.meta.r_max = np.max(np.sqrt(x**2 + y**2))
+        sixdb_da.meta.ang_min = 0
+        sixdb_da.meta.ang_max = 90
+        sixdb_da.meta.npart = len(sixdb_da._surv.index)
+    
+    sixdb_da.meta._store()
+    return sixdb_da
+# --------------------------------------------------------
