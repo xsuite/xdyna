c0e3f54c071bd6b5c37c009a6ba00781e73436a5
Frederik Van der Veken <frederik@cern.ch>
2023-09-16 00:27:09 +0200

    Added protection hook installation

--- xdyna/__init__.py
+++ xdyna/__init__.py
@@ -1,8 +1,9 @@
-from .general import _pkg_root
-from .da import DA
-from .da_meta import regenerate_meta_file
-from .protectfile import ProtectFile, get_hash
+# copyright ############################### #
+# This file is part of the Xdyna Package.   #
+# Copyright (c) CERN, 2023.                 #
+# ######################################### #
+
+from .general import _pkg_root, __version__
 
 from .plot import plot_particles, plot_da_border, plot_da_border, plot_davsturns_border
 
-__version__ = '0.1.1'
--- xdyna/general.py
+++ xdyna/general.py
@@ -1,3 +1,14 @@
+# copyright ############################### #
+# This file is part of the Xdyna Package.   #
+# Copyright (c) CERN, 2023.                 #
+# ######################################### #
+
 from pathlib import Path
 
 _pkg_root = Path(__file__).parent.absolute()
+
+# ===================
+# Do not change
+# ===================
+__version__ = '0.1.1'
+# ===================
